f8e892f8d74a3232301c9ab01426850167437267
arialblack14 <tsirasalexis@yahoo.gr>
2017-01-05 20:32:50 +0200

    Improve feature creating_homepage

--- spec/features/crating_homepage_spec.rb
+++ spec/features/crating_homepage_spec.rb
@@ -2,13 +2,13 @@ require 'rails_helper'
 
 RSpec.feature "Creating Homepage" do
 
-  scenario "A user visits the homepage" do
-    visit "/"
+  scenario do
+    visit '/'
 
-    expect(page).to have_content("Home")
-    expect(page).to have_content("Athletes Den")
-    expect(page).to have_content("Workout Lounge!")
-    expect(page).to have_content("Show off your workout")
+    expect(page).to have_content('Home')
+    expect(page).to have_content('Athletes Den')
+    expect(page).to have_content('Workout Lounge!')
+    expect(page).to have_content('Show off your workout')
   end
 
 end
