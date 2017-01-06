93948b00d269df2bd4b38ec21f2ef903e2a1d76b
arialblack14 <tsirasalexis@yahoo.gr>
2017-01-06 13:52:15 +0200

    implement homepage

--- spec/features/crating_homepage_spec.rb
+++ spec/features/crating_homepage_spec.rb
@@ -5,8 +5,8 @@ RSpec.feature "Creating Homepage" do
   scenario do
     visit '/'
 
-    expect(page).to have_content('Home')
-    expect(page).to have_content('Athletes Den')
+    expect(page).to have_link('Home')
+    expect(page).to have_link('Athletes Den')
     expect(page).to have_content('Workout Lounge!')
     expect(page).to have_content('Show off your workout')
   end
