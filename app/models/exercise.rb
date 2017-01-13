c3b8b68fdba6c31dc6a8dc3e34f009cebf754852
arialblack14 <tsirasalexis@yahoo.gr>
2017-01-13 12:04:39 +0200

    create exercise unsuccessfully

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,3 +1,10 @@
 class Exercise < ApplicationRecord
   belongs_to :user
+  # to match the names we have on our exercises form
+  alias_attribute :workout_details, :workout
+  alias_attribute :activity_date, :workout_date
+
+  validates :duration_in_min, numericality: { grater_than: 0.0 }
+  validates :workout_details, presence: true
+  validates :activity_date, presence: true
 end
