ccb01d19ab3e90e144c5a820d07ad45cd03f74ae
arialblack14 <tsirasalexis@yahoo.gr>
2017-01-29 17:31:51 +0200

    implement messages controller

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -6,8 +6,11 @@ class ExercisesController < ApplicationController
     # displaying last weeks workouts in desc order
     # @exercises = current_user.exercises.last_week.desc_date
     @exercises = current_user.exercises
-
     @friends = current_user.friends
+    set_current_room
+    @message = Message.new
+    @messages = current_room.messages if current_room
+    @followers = Friendship.where(friend_id: current_user.id)
   end
 
   def show
@@ -61,5 +64,14 @@ class ExercisesController < ApplicationController
 
   def exercise_params
     params.require(:exercise).permit(:duration_in_min, :workout, :workout_date, :user_id)
+  end
+
+  def set_current_room
+    if params[:roomId]
+      @room = Room.find_by(params[:roomId])
+    else
+      @room = current_user.room
+    end
+    session[:current_room] = @room.id if @room
   end
 end
